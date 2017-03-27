06ecbed3987a9d13d4565626aed87111a9d7df90
Dominik Adamski <dominik.adamski@interia.pl>
2017-03-28 00:46:51 +0200

    test

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -39,19 +39,34 @@ function businesscard()
 	navigator.notification.alert(info);
 }
 
-function network() 
-{
-    var networkState = navigator.connection.type;
+ function onDeviceReady() {
+        // find all contacts
+        var options = new ContactFindOptions();
+        options.filter = "";
+        var filter = ["displayName", "addresses"];
+        navigator.contacts.find(filter, onSuccess, onError, options);
+    }
 
-    var states = {};
-    states[Connection.UNKNOWN]  = 'Unknown connection';
-    states[Connection.ETHERNET] = 'Ethernet connection';
-    states[Connection.WIFI]     = 'WiFi connection';
-    states[Connection.CELL_2G]  = 'Cell 2G connection';
-    states[Connection.CELL_3G]  = 'Cell 3G connection';
-    states[Connection.CELL_4G]  = 'Cell 4G connection';
-    states[Connection.CELL]     = 'Cell generic connection';
-    states[Connection.NONE]     = 'No network connection';
+    // onSuccess: Get a snapshot of the current contacts
+    //
+    function onSuccess(contacts) {
+        // display the address information for all contacts
+        for (var i = 0; i < contacts.length; i++) {
+            for (var j = 0; j < contacts[i].addresses.length; j++) {
+                alert("Pref: "           + contacts[i].addresses[j].pref          + "\n" +
+                      "Type: "           + contacts[i].addresses[j].type          + "\n" +
+                      "Formatted: "      + contacts[i].addresses[j].formatted     + "\n" +
+                      "Street Address: " + contacts[i].addresses[j].streetAddress + "\n" +
+                      "Locality: "       + contacts[i].addresses[j].locality      + "\n" +
+                      "Region: "         + contacts[i].addresses[j].region        + "\n" +
+                      "Postal Code: "    + contacts[i].addresses[j].postalCode    + "\n" +
+                      "Country: "        + contacts[i].addresses[j].country);
+            }
+        }
+    };
 
-    alert('Connection type: ' + states[networkState]);
-}
+    // onError: Failed to get the contacts
+    //
+    function onError(contactError) {
+        alert('onError!');
+    }
