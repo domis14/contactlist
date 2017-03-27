62273b04706c20ba4ec17280089fb683d4ec97d6
Dominik Adamski <dominik.adamski@interia.pl>
2017-03-27 19:50:14 +0200

    zmiana funkcji internet

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -39,10 +39,19 @@ function businesscard()
 	navigator.notification.alert(info);
 }
 
-function network()
+function network() 
 {
-	info = 'Informacje dotyczace połączenia z internetem \n' +
-			'Device Model   : '    + device.model     + '\n' + 
-			'Device Version : '  + device.version  + '\n';
-	navigator.notification.alert(info);
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+
+    alert('Connection type: ' + states[networkState]);
 }
